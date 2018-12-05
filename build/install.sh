1b75659cac4c0e5638a7ca914b2747508b0d166e
Paul Scarrone <ninjapanzer@gmail.com>
2018-12-04 21:32:39 -0500

    add basic build script

---
+++ build/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Deps
+ sudo yum install -y \
+  autoconf \
+  libxml2-devel \
+  make \
+  automake \
+  gcc \
+  gcc-c++ \
+  kernel-devel;
+
+# PHP local
+PHP_INI_SCAN_DIR=/home/ec2-user/php.d \
+EXTENSION_DIR=/home/ec2-user/php_phalcon/lib/php/extensions/no-debug-non-zts-20170718 \
+./configure --with-config-file-scan-dir=/home/ec2-user/php_phalcon/php.d --prefix=/home/ec2-user/php_phalcon;
+
+make;
+make install;
+
+# CPhalcon Ext
+sudo ./install --phpize /home/ec2-user/php_phalcon/bin/phpize --php-config /home/ec2-user/php_phalcon/bin/php-config                               
+
+cd php_phalcon;
+mkdir php.d;
+touch 40-phalcon.ini;
+echo "extension=phalcon" >> 40-phalcon.ini;
+
+zip -r php_info.zip php_phalcon bootstrap
